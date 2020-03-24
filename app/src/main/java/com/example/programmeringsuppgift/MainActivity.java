ea99a3a675d1dbcda5680bf5d2118e2d36acf77c
Tove <toveso97@hotmail.com>
2020-03-24 14:03:17 +0100

    Added buttons for accelerometer and compass. Activities added + navigation to go back to main activity

--- app/src/main/java/com/example/programmeringsuppgift/MainActivity.java
+++ app/src/main/java/com/example/programmeringsuppgift/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.programmeringsuppgift;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+    }
+
+    /** Called when the user taps the Acceleromater button*/
+    public void start_accelerometer(View view) {
+        Intent intent = new Intent(this, AccelerometerActivity.class);
+
+        startActivity(intent);
+    }
+    /** Called when the user taps the Compass button*/
+    public void start_compass(View view) {
+        Intent intent = new Intent(this, CompassActivity.class);
+
+        startActivity(intent);
     }
 }
