095a65c507804fc071db8b966bf24997798a2924
Tove <toveso97@hotmail.com>
2020-03-24 15:48:40 +0100

    accelerometer is working

--- app/src/main/java/com/example/programmeringsuppgift/AccelerometerActivity.java
+++ app/src/main/java/com/example/programmeringsuppgift/AccelerometerActivity.java
@@ -1,16 +1,46 @@
 package com.example.programmeringsuppgift;
 
+import android.hardware.Sensor;
 import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class AccelerometerActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
+
+public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private TextView textViewX, textViewY, textViewZ;
+    private static DecimalFormat df = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        textViewX = findViewById(R.id.textViewX);
+        textViewY = findViewById(R.id.textViewY);
+        textViewZ = findViewById(R.id.textViewZ);
     }
-    
+
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        textViewX.setText("X: " + df.format(event.values[0]));
+        textViewY.setText("Y: " + df.format(event.values[1]));
+        textViewZ.setText("Z: " + df.format(event.values[2]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
 }
